35daee46aed92d565ae483101998579ccd403126
Kritika Dhiman <a00268745@student.ait.ie>
2022-01-29 23:24:22 +0000

    open phone and application view

--- app/src/main/java/com/example/renttrackerapp/HomeDetailActivity.kt
+++ app/src/main/java/com/example/renttrackerapp/HomeDetailActivity.kt
@@ -1,8 +1,9 @@
 package com.example.renttrackerapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.example.renttrackerapp.databinding.ActivityHomeDetailBinding
 import com.example.renttrackerapp.model.Home
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -22,18 +23,45 @@ class HomeDetailActivity : AppCompatActivity() {
 
         val home = intent.getSerializableExtra("itemDetail") as? Home
 
-        lifecycleScope.launchWhenResumed {
-            val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
-            mapFragment.getMapAsync { mapGoogle ->
-                // Add a marker in Sydney and move the camera
-                home?.latitude?.toDouble()?.let { lat ->
-                    val location = LatLng(lat, home.longitude.toDouble())
-                    mapGoogle.addMarker(MarkerOptions().position(location).title("House Location"))
-                    mapGoogle.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 14.0f))
-                }
-
+        supportActionBar?.title = ""
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.titleTextView.text = home?.title
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync { mapGoogle ->
+            home?.latitude?.toDouble()?.let { lat ->
+                val location = LatLng(lat, home.longitude.toDouble())
+                mapGoogle.addMarker(MarkerOptions().position(location).title("House Location"))
+                mapGoogle.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 14.0f))
             }
         }
+        binding.phoneTextView.apply {
+            text = home?.phone
+            setOnClickListener {
+                home?.phone?.let { it1 -> callPhone(it1) }
+            }
+        }
+        binding.daftLink.setOnClickListener {
+            home?.link?.let { it1 -> openApp(it1) }
+        }
+        binding.rentTextView.text = home?.rent
+        binding.descriptionTextView.text = home?.description
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    private fun callPhone(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_DIAL)
+        intent.data = Uri.parse("tel:$phoneNumber")
+        startActivity(intent)
+    }
+
+    private fun openApp(link: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(link)
+        startActivity(intent)
     }
 }
